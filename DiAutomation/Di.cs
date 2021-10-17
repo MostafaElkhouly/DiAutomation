477c82b0673e0c201d50629cc3e3902dab8bfcb4
Mostafa Anwar <Mostafa.anwar.gawad@gmail.com>
2021-10-17 02:22:40 +0200

    init DiAutomation

---
+++ DiAutomation/Di.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiAnnotations
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class Di : Attribute
+    {
+        public Di()
+        {
+
+        }
+
+        public Type Imp { set; get; }
+        public Type Int { set; get; }
+        public RequestType RequestType { set; get; } = RequestType.Transient;
+        public bool IsActive { set; get; } = true;
+    }
+
+    public enum RequestType
+    {
+        Singletone,
+        Scoped,
+        Transient
+    }
+}
---
+++ DiAutomation/ExecuteDi.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiAnnotations
+{
+    public static class ExecuteDi
+    {
+        public static void ExecuteDI(this IServiceCollection services, Assembly[] assemblies)
+        {
+            
+            string[] filePaths = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.dll",
+                                         SearchOption.AllDirectories);
+
+            List<Type> types = new List<Type>();
+
+            foreach(string dll in filePaths)
+            {
+                Assembly assembly = null;
+                try
+                {
+                    assembly = Assembly.LoadFrom(dll);
+                }
+                catch(System.BadImageFormatException ex)
+                {
+                    continue;
+                }
+
+                var type1Types =
+                        (from type in assembly.GetTypes()
+                        where type.IsDefined(typeof(Di), true)
+                        select type).ToList();
+
+                types.AddRange(type1Types);
+            }
+
+            foreach (var item in types)
+            {
+
+                Di di =
+                    (Di)Attribute.GetCustomAttribute(item, typeof(Di));
+                services.AddTransient((Type)di.Int, (Type)di.Imp);
+            }
+        }
+    }
+}
